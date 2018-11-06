82cc91ffffb60a9f914962a0c42655c87b499d7f
dev <bene.a99@gmail.com>
2018-11-06 14:42:26 +0100

    added classes

---
+++ app/src/main/java/se/shitchat/shitchatapp/Chat.java
@@ -0,0 +1,11 @@
+package se.shitchat.shitchatapp;
+
+
+/**
+ * Innehåller all info för en chat
+ */
+public class Chat {
+
+    private User[] members;
+    private String name;
+}
---
+++ app/src/main/java/se/shitchat/shitchatapp/User.java
@@ -0,0 +1,4 @@
+package se.shitchat.shitchatapp;
+
+public class User {
+}
