e0ded887255c4b2b294e9b7da0020c9a6c87ba41
bejan <beyhanbresner@gmail.com>
2018-11-06 13:21:21 +0100

    Alla fragments skapade inuti fragment mapp

---
+++ app/src/main/java/se/shitchat/shitchatapp/fragments/InfoFragment.java
@@ -0,0 +1,28 @@
+package se.shitchat.shitchatapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import se.shitchat.shitchatapp.R;
+
+
+public class InfoFragment extends Fragment {
+
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+}
