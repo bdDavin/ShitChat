f9bb9c6f2e1a0a8be060290ed711fa399bb14664
Benjamin <bene.a99@gmail.com>
2018-11-10 17:05:23 +0100

    displays username in profile activity

--- app/src/main/java/se/shitchat/shitchatapp/ProfileActivity.java
+++ app/src/main/java/se/shitchat/shitchatapp/ProfileActivity.java
@@ -3,6 +3,9 @@ package se.shitchat.shitchatapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -15,9 +18,13 @@ public class ProfileActivity extends AppCompatActivity {
 
         t = findViewById(R.id.profileToolbar);
         setSupportActionBar(t);
+        TextView userName = findViewById(R.id.username_text);
+
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //displays username
+        userName.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
     }
 }
