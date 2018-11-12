1f808d97fc00150428ba07ee7fcf7b599c89f1c6
Benjamin <bene.a99@gmail.com>
2018-11-12 13:08:45 +0100

    can send message to firestore

--- app/src/main/java/se/shitchat/shitchatapp/ProfileActivity.java
+++ app/src/main/java/se/shitchat/shitchatapp/ProfileActivity.java
@@ -3,6 +3,7 @@ package se.shitchat.shitchatapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,6 +20,7 @@ public class ProfileActivity extends AppCompatActivity {
         t = findViewById(R.id.profileToolbar);
         setSupportActionBar(t);
         TextView userName = findViewById(R.id.username_text);
+        ImageView userImage = findViewById(R.id.profile_image);
 
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -26,5 +28,12 @@ public class ProfileActivity extends AppCompatActivity {
 
         //displays username
         userName.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+        //displays user image
+        if(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl() != null ) {
+            //TODO load in user image
+        }
+        else {
+            userImage.setImageResource(R.drawable.default_profile);
+        }
     }
 }
